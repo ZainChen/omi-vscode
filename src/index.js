2a29d211c215fd58bf998a58745bbd99e1d0a9d6
ZainChen <2384439266@qq.com>
2019-02-19 20:44:25 +0800

    add registerCompletionItemProvider

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ function activate(context) {
 	context.subscriptions.push(vscode.commands.registerCommand('command.omi', cmd.commandOmi));  //omi命令注册
 	context.subscriptions.push(vscode.languages.registerHoverProvider(['json', 'javascript'], {provideHover}));  //鼠标悬停提示注册
     context.subscriptions.push(vscode.languages.registerDefinitionProvider(['json', 'javascript'], {provideDefinition}));  //跳转到定义注册
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider(['json', 'javascript'], {provideCompletionItems, resolveCompletionItem}, '.'));  //代码提示功能2注册
 	
 }
 exports.activate = activate;
@@ -41,3 +42,41 @@ function deactivate() {
 
 
 
+
+
+/**
+ * (未出效果，待解决...)
+ * 自动提示实现，这里模拟一个很简单的操作
+ * 当输入 this.dependencies.xxx时自动把package.json中的依赖带出来
+ * 当然这个例子没啥实际意义，仅仅是为了演示如何实现功能
+ * @param {*} document 
+ * @param {*} position 
+ * @param {*} token 
+ * @param {*} context 
+ */
+function provideCompletionItems(document, position, token, context) {
+    const line        = document.lineAt(position);
+    const projectPath = __dirname;
+
+    // 只截取到光标位置为止，防止一些特殊情况
+    const lineText = line.text.substring(0, position.character);
+    // 简单匹配，只要当前光标前的字符串为`this.dependencies.`都自动带出所有的依赖
+    if(/(^|=| )\w+\.dependencies\.$/g.test(lineText)) {
+        const json = require(`${projectPath}/package.json`);
+		const dependencies = Object.keys(json.dependencies || {}).concat(Object.keys(json.devDependencies || {}));
+		//return new vscode.CompletionItem(dependencies[0], vscode.CompletionItemKind.Field);
+        return dependencies.map( (dep) => {
+            // vscode.CompletionItemKind 表示提示的类型
+            return new vscode.CompletionItem(dep, vscode.CompletionItemKind.Field);
+        })
+    }
+}
+
+/**
+ * 光标选中当前自动补全item时触发动作，一般情况下无需处理
+ * @param {*} item 
+ * @param {*} token 
+ */
+function resolveCompletionItem(item, token) {
+    return item;
+}
